dd861ad24cecbe143dd8257447e4dce72562669c
Sangwon Hong <qpakzk@gmail.com>
2018-05-20 20:58:31 +0900

    Implement the genesis block
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- src/blockchain.js
+++ src/blockchain.js
@@ -7,3 +7,15 @@ class Block {
         this.data = data;
     }
 }
+
+const genesisBlock = new Block(
+    0,
+    'AC5C3584F9E07BA287B9237AB67673FF73C98BE02B3EAC000D9FFC263CBEDFCB',
+    null,
+    1526817148049,
+    "This is the genesis block!!",
+);
+
+let blockchain = [genesisBlock];
+
+console.log(blockchain)
