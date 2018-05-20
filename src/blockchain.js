7b9fb8a7ca82a90f79640c6225d03c6f4dce73df
Sangwon Hong <qpakzk@gmail.com>
2018-05-20 22:01:51 +0900

    check validation
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- src/blockchain.js
+++ src/blockchain.js
@@ -23,7 +23,7 @@ let blockchain = [genesisBlock];
 const getLastBlock = () => blockchain[blockchain.length - 1];
 const getTimestamp = () => new Date().getTime() / 1000;
 const createHash = (index, previousHash, timestamp, data) => 
-    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    CryptoJS.SHA256(index + previousHash + timestamp + JSON.stringify(data)).toString();
 const createNewBlock = data => {
     const previousBlock = getLastBlock();
     const newBlockIndex = previousBlock.index + 1;
@@ -49,7 +49,11 @@ const createNewBlock = data => {
 const getBlocksHash = (block) => createHash(block.index, block.previousHash, block.timestamp, block.data);
 
 const isNewBlockValid = (candidateBlock, lastestBlock) => {
-    if(lastestBlock.index + 1 !== candidateBlock.index) {
+    if(!isNewStructureValid(candidateBlock)) {
+        console.log("The candidate block is not valid.");
+        return false;
+    }
+    else if(lastestBlock.index + 1 !== candidateBlock.index) {
         console.log("The candidate block doesn't have a valid block.");
         return false;
     }
@@ -62,5 +66,32 @@ const isNewBlockValid = (candidateBlock, lastestBlock) => {
         return false;
     }
 
+    return true;
+}
+
+const isNewStructureValid = block => {
+    return (
+        typeof block.index === 'number' &&
+        typeof block.hash === 'string' &&
+        typeof block.previousHash === 'string' &&
+        typeof block.timestamp === 'number' &&
+        typeof block.data === 'string'
+    )
+}
+
+const isChainValid = candidateChain => {
+    const isGenesisValid = block => {
+        return JSON.stringify(block) === JSON.stringify(genesisBlock);
+    }
+
+    if(!isGenesisValid(candidateChain[0])) {
+        console.log("The candidateChain's genesisBlock is not the same as our genesisBlock.");
+        return false;
+    }
+
+    for(let i = 1; i < candidateChain.length; i++) {
+        if(!isNewBlockValid(candidateChain[i], candidateChain[i - 1])) 
+            return false;
+    }
     return true;
 }
